cdc1ca2ae8e67e34bc914c6dcc8684f028ad40a3
Roman Geathers <romangeathersdev@gmail.com>
2022-05-16 20:49:41 -0400

    Reset CSS and produce grid squares
    Reset all CSS styles.
    Write function that produces 256 grid squares for grid.

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 let grid = document.querySelector(".grid");
 
-for (let i = 0; i < 16; i++) {
-    let gridSquare = document.createElement("div");
-    gridSquare.classList.add("grid-square");
-    grid.appendChild("gridSquare");
+function produceGridSquares() {
+    for (let i = 0; i < 256; i++) {
+        let gridSquare = document.createElement("div");
+        gridSquare.classList.add("grid-square");
+        grid.appendChild(gridSquare);
+    }
 }
+
+produceGridSquares();
