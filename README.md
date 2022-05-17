8929ce4e6bda17acc12b81ad1d2e14de31e8c210
Roman Geathers <romangeathersdev@gmail.com>
2022-05-16 20:33:12 -0400

    Set up files and begin implmenting grid creation
    Set up HTMl, CSS, and JS files for application.
    Start working on creating 16x16 grid using javascript

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+This application is a mix between a sketchpad and an "etch-a-sketch."
---
+++ script.js
@@ -0,0 +1,7 @@
+let grid = document.querySelector(".grid");
+
+for (let i = 0; i < 16; i++) {
+    let gridSquare = document.createElement("div");
+    gridSquare.classList.add("grid-square");
+    grid.appendChild("gridSquare");
+}
